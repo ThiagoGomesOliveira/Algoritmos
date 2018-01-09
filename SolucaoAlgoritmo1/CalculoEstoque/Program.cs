08260f5db92c6a2966f19e5e0e75d51c26f8ac32
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-09 17:42:03 -0200

    thiago Gomes oliveira
    Calculo estoque

--- SolucaoAlgoritmo1/CalculoEstoque/Program.cs
+++ SolucaoAlgoritmo1/CalculoEstoque/Program.cs
@@ -11,7 +11,28 @@ namespace CalculoEstoque
         static void Main(string[] args)
         {
 
-            
+            Console.WriteLine("Digite o número do codigo da peça");
+            var numero = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor da peça");
+            var valor = Console.ReadLine();
+
+            Console.WriteLine("Quantidade de peças");
+            var qtd = Console.ReadLine();
+
+            Console.WriteLine("Calculo do valor total da peça ");
+            int quantidade = Convert.ToInt32(qtd);
+            int valorQtd = Convert.ToInt32(valor);
+            var total = quantidade * valorQtd;
+
+            Console.WriteLine("Valor total da peça "+ total);
+
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
