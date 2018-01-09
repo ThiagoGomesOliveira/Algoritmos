93b875feeb6053441ab1111a46ecf7804152db20
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-09 18:14:15 -0200

    thiago gomes oliveira
    Estoque Médio

---
+++ SolucaoAlgoritmo1/EstoqueMedio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueMedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número da peça");
+            var num = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade minima de estoque para a peça");
+            var min = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade maxima do estoque para a peça");
+            var max = Console.ReadLine();
+
+            int minQtd = Convert.ToInt32(min);
+            int maxQtd = Convert.ToInt32(max);
+
+            double media = (minQtd + maxQtd) / 2;
+
+            Console.WriteLine("Media estoque da peça " + num +" : "+media);
+
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
