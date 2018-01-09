8c3b3753af81e7b231920ea3090b8c024ee0d95f
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-09 18:44:01 -0200

    thiago gomes oliveira

---
+++ ProcessoCondicional/SeEstiverChovendo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeEstiverChovendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Se hoje estiver chovendo ?..... 1:Sim  ou  2:Não.....");
+            var res = Console.ReadLine();
+            int resposta = Convert.ToInt16(res);
+
+            var situacao = resposta == 1 ? " Vou ver um Filme." : "  Vou Caminhar.";
+
+            Console.WriteLine(" Se hoje estiver chovendo " + situacao);
+            Console.ReadKey();
+
+        }
+    }
+}
