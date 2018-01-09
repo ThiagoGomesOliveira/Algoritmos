ba1eff04bea1458d68873e906fda2f620e24d10e
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-09 18:29:58 -0200

    thiago gomes oliveira
    Número maior que 10

---
+++ ProcessoCondicional/VerificarNumeroMaiorQue10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarNumeroMaiorQue10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Faça um algoritmo que leia um número e verefique se ele é maior do que 10.
+        //    Se for o algoritmo deve somar mais 10 as esse número.
+
+            Console.WriteLine("Digite um número");
+            var numero = Console.ReadLine();
+
+            int num = Convert.ToInt16(numero);
+
+            int total = num>10 ? num+10 :num;
+
+            Console.WriteLine("Total "+ total);
+           
+        }
+    }
+}
