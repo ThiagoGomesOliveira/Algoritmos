c6bf170f22f1c0cde63d562ee76f64d64c2a62a8
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-10 19:32:05 -0200

    thiago gomes oliveira
    Número maior que 20

---
+++ ProcessoCondicional/numeroMaiorVinte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroMaiorVinte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            var numero = Console.ReadLine();
+            int num = Convert.ToInt32(numero.ToString());
+            MaiorQueVinte(num);
+        }
+        public static void MaiorQueVinte(int num)
+        {
+
+            if (num > 20 )
+            {
+                Console.WriteLine("Número Digitado maior que 20: "+num);
+                Console.ReadKey();
+            }
+            else if(num ==20)
+            {
+                Console.WriteLine("Número Digitado é 20 " );
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Número Digitado menor que 20 : " + num);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
