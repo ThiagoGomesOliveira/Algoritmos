6d95307342b9b939ae6be4b5b1e4990d0c73e919
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-01-28 17:07:55 -0200

    ProdutoComCinquentaOuTrintaPorcentoDesconto

---
+++ ProcessoCondicional/ProdutoComLucro/Classes/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoComLucro.Classes
+{
+    public class Produto
+    {
+        public long Valor { get; set; }
+        public string Nome { get; set; }
+
+        //public Produto(long valor , string nome)
+        //{
+        //    this.Valor = valor;
+        //    this.Nome = nome;
+        //}
+
+        private double CalcularLucroProduto(long valor)
+        {
+
+            if (valor < 20)
+            {
+              var produtoPorcentagem = PorcentagemProdutoCinquentaPorCento();
+                return produtoPorcentagem;
+            }
+            var produtoPorc = PorcentagemProdutoTrintaECincoPorcento();
+            return produtoPorc;
+
+        }
+
+        public double PorcentagemProdutoCinquentaPorCento()
+        {
+            
+            var porcentagem  = this.Valor * 0.5;
+            return porcentagem + this.Valor;
+        }
+        public double PorcentagemProdutoTrintaECincoPorcento()
+        {
+            double produto = this.Valor * 0.35;
+            return produto + this.Valor;
+        }
+        public double ExecutarCalculoProduto(long valor)
+        {
+            var  produtoReajuste =  CalcularLucroProduto(valor);
+            return produtoReajuste;
+        }
+
+    }
+}
---
+++ ProcessoCondicional/ProdutoComLucro/Program.cs
@@ -0,0 +1,32 @@
+﻿using ProdutoComLucro.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProdutoComLucro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Nome do Produto");
+            string nomeProduto = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Digite o Valor do Produto");
+             var valor = Console.ReadLine();
+             var valorProduto = Convert.ToInt64(valor);
+
+            Produto produto = new Produto();
+            produto.Valor = valorProduto;
+           
+            Console.WriteLine("O Valor do Produto "+ produto.ExecutarCalculoProduto(valorProduto));
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
